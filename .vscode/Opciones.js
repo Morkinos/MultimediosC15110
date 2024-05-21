e9af2ebd610fc83406340221b49184b51a472e22
Emmanuel Mora Miranda <emorita188@gmai.com>
2024-05-21 02:55:53 -0600

    Proyecto correciones
    Corregimos el navbar y el index

--- .vscode/Opciones.js
+++ .vscode/Opciones.js
@@ -40,10 +40,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 </li>
             </ul>
         </nav>
-        <a class="btn" onclick="cerrarSesion()"><button class="btn_button">Cerrar Sesión</button></a>
+        <a class="btn" href="login.html" onclick="cerrarSesion()"><button class="btn_button">Cerrar Sesión</button></a>
         <a onclick="openNav()" class="menu" href="#"><button class="menu_button">Menu</button></a>
         <div id="mobile-menu" class="overlay">
-            <a onclick="closeNav()" href="#" class="close">&times;</a>
+            <a onclick="closeNav()" href="" class="close">&times;</a>
             <div class="overlay-content">
                 <a href="Index.html">Inicio</a>
                 <ul>
@@ -51,14 +51,14 @@ document.addEventListener("DOMContentLoaded", function () {
                         <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Profesores</a>
                         <div class="dropdown-menu">
                             <a class="dropdown-item" href="profesores.html">Lista</a>
-                            <a class="dropdown-item" href="profesoresCrear.html">Crear</a>
+                            <a class="dropdown-item" href="profesoresCrear">Crear</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Estudiantes</a>
                         <div class="dropdown-menu">
                             <a class="dropdown-item" href="Estudiantes.html">Lista</a>
-                            <a class="dropdown-item" href="EstudiantesCrear.html">Crear</a>
+                            <a class="dropdown-item" href="estudiantesCrear.html">Crear</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
@@ -76,7 +76,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         </div>
                     </li>
                 </ul>
-                <a class="btn" onclick="cerrarSesion()"><button class="btn_button">Cerrar Sesión</button></a>
+                <a class="btn" href="login.html" onclick="cerrarSesion()"><button class="btn_button">Cerrar Sesión</button></a>
             </div>
         </div>`;
 
@@ -131,19 +131,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("piedepagina").innerHTML = piedepagina;
 
-    window.openNav = function() {
+    function openNav() {
         document.getElementById("mobile-menu").style.width = "100%";
-    };
+    }
 
-    window.closeNav = function() {
+    function closeNav() {
         document.getElementById("mobile-menu").style.width = "0%";
-    };
+    }
 
-    window.cerrarSesion = function() {
-        console.log('Attempting to log out');
-        console.log('Current token:', sessionStorage.getItem("token"));
+    function cerrarSesion() {
+        console.log('borr');
+        console.log(sessionStorage.getItem("token"));
         sessionStorage.removeItem("token");
-        window.location.href ="login.html"
-        console.log('Token after removal:', sessionStorage.getItem("token"));
-    };
+    }
 });
--- Proyecto/js/login.js
+++ Proyecto/js/login.js
@@ -1,130 +1,168 @@
-
 let url = "https://paginas-web-cr.com/Api/apis/";
 let autenticar = "AutenticarUsuario.php";
+let recuperar = "SendPassword.php";
 
 let nombrePagina = document.title;
 let loginPagina = "Login";
-let spinnerText = 'Cargando'
+let recovery = "Recuperar Password";
+let spinnerText = 'Cargando';
 let spinner = `
-            <button
-            class="btn btn-primary"
-            type="button"
-            disabled
-            >
-            <span
-                class="spinner-border spinner-border-sm"
-                role="status"
-                aria-hidden="true"
-            ></span>
-            ${spinnerText}
-            </button>`;
-
-const showHiddenPass = (loginpass, loginEye) => {
-    const input = document.getElementById(loginpass);
-    const iconEye = document.getElementById(loginEye);
-
-    iconEye.addEventListener('click', () => {
-
-        //cambiamos la contrase;a a tipo texto
-        if (input.type === 'password') {
-            //cambiamos 
-            input.type = 'text';
-            //cambio de icooono
-            iconEye.classList.add('ri-eye-fill')
-            iconEye.classList.remove('ri-eye-off-fill')
-        } else {
-            input.type = 'password';
-            //cambio de icooono
-            iconEye.classList.add('ri-eye-off-fill')
-            iconEye.classList.remove('ri-eye-fill')
-        }
-    });
-}
-
+    <button class="btn btn-primary" type="button" disabled>
+        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+        ${spinnerText}
+    </button>`;
 
 if (nombrePagina == loginPagina) {
+    const showHiddenPass = (loginpass, loginEye) => {
+        const input = document.getElementById(loginpass);
+        const iconEye = document.getElementById(loginEye);
 
-    formularioLogin.addEventListener("submit",
-        function (evento) {
+        iconEye.addEventListener('click', () => {
+            // Cambiamos la contraseña a tipo texto
+            if (input.type === 'password') {
+                input.type = 'text';
+                // Cambio de icono
+                iconEye.classList.add('ri-eye-fill')
+                iconEye.classList.remove('ri-eye-off-fill')
+            } else {
+                input.type = 'password';
+                // Cambio de icono
+                iconEye.classList.add('ri-eye-off-fill')
+                iconEye.classList.remove('ri-eye-fill')
+            }
+        });
+    }
 
-            evento.preventDefault();//evita la recarga de la pagina
+    document.getElementById('formularioLogin').addEventListener("submit", function (evento) {
+        evento.preventDefault(); // Evita la recarga de la pagina
 
-            let datos = new FormData(formularioLogin);
+        let datos = new FormData(document.getElementById('formularioLogin'));
 
-            let datosEnviar =
-            {
-                email: datos.get('loginemail'),
-                password: datos.get('loginpass')
-            };
+        let datosEnviar = {
+            email: datos.get('loginemail'),
+            password: datos.get('loginpass')
+        };
 
-            fetch(url + autenticar,
-                {
-                    method: 'POST',
-                    body: JSON.stringify(datosEnviar)
-                }
-            )
-                .then(repuesta => repuesta.json())
-                .then((datosrespuestas) => {
-
-                    if (datosrespuestas.code === 200) {
-                        
-                        // Guarda el token de sesión en Storage
-                        sessionStorage.setItem('token', datosrespuestas.data);
-                        
-                    }
-                    insertarDatosLogin(datosrespuestas);
-                })
+        fetch(url + autenticar, {
+            method: 'POST',
+            body: JSON.stringify(datosEnviar)
         })
-}
-//Emmanuel@gmail.com
+        .then(repuesta => repuesta.json())
+        .then((datosrespuestas) => {
+            if (datosrespuestas.code === 200) {
+                // Guarda el token de sesión en Storage
+                sessionStorage.setItem('token', datosrespuestas.data);
+            }
+            insertarDatosLogin(datosrespuestas);
+        })
+    });
 
-function insertarDatosLogin(datosrespuestas) {
-    // Carga el spinner cuando se llama a la función
-    cargarSpinner();
+    function insertarDatosLogin(datosrespuestas) {
+        // Carga el spinner cuando se llama a la función
+        cargarSpinner();
 
-    // Espera 1 segundo antes de realizar otras operaciones
-    setTimeout(() => {
-        if (datosrespuestas.code == 200) {
-            // Cambia el texto del spinner a "Bienvenido"
-            spinnerText = 'Bienvenido';
-            // Renderiza el spinner con el nuevo texto
-            cargarSpinner();
-            // Espera 2 segundos antes de redirigir
-            setTimeout(() => {
-                window.location.href = 'index.html';
-            }, 1500);
-        } else {
-            // Muestra el mensaje de advertencia cuando el inicio de sesión falla
-            message.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                <strong>Correo/Contraseña incorrectos</strong>
-            </div>`;
-            // Oculta el spinner después de mostrar el mensaje de advertencia
-            ocultarSpinner();
-            // Limpia los campos de email y contraseña
-            document.getElementById('loginpass').value = '';
-            document.getElementById('loginemail').value = '';
-        }
-    }, 1000);
-}
+        // Espera 1 segundo antes de realizar otras operaciones
+        setTimeout(() => {
+            if (datosrespuestas.code == 200) {
+                // Cambia el texto del spinner a "Bienvenido"
+                spinnerText = 'Bienvenido';
+                // Renderiza el spinner con el nuevo texto
+                cargarSpinner();
+                // Espera 2 segundos antes de redirigir
+                setTimeout(() => {
+                    window.location.href = 'index.html';
+                }, 1500);
+            } else {
+                // Muestra el mensaje de advertencia cuando el inicio de sesión falla
+                document.getElementById('mensajes').innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    <strong>Correo/Contraseña incorrectos</strong>
+                </div>`;
+                // Oculta el spinner después de mostrar el mensaje de advertencia
+                ocultarSpinner();
+                // Limpia los campos de email y contraseña
+                document.getElementById('loginpass').value = '';
+                document.getElementById('loginemail').value = '';
+            }
+        }, 1000);
+    }
 
-
-function cargarSpinner() {
-    // Actualiza el HTML del spinner con el valor actual de spinnerText
-    document.getElementById("seccionSpinner").innerHTML = `
+    function cargarSpinner() {
+        // Actualiza el HTML del spinner con el valor actual de spinnerText
+        document.getElementById("seccionSpinner").innerHTML = `
         <button class="btn btn-primary" type="button" disabled>
             <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
             ${spinnerText}
         </button>`;
-}
+    }
 
-function ocultarSpinner() {
-    // Oculta el spinner
-    document.getElementById("seccionSpinner").innerHTML = '';
-}
-function cambiarTextoSpinner(texto) {
-    spinnerText = texto;
-    cargarSpinner(); // Vuelve a renderizar el spinner con el nuevo texto
-}
+    function ocultarSpinner() {
+        // Oculta el spinner
+        document.getElementById("seccionSpinner").innerHTML = '';
+    }
 
-showHiddenPass('loginpass', 'logineye')
+    function cambiarTextoSpinner(texto) {
+        spinnerText = texto;
+        cargarSpinner(); // Vuelve a renderizar el spinner con el nuevo texto
+    }
+
+    // Asegúrate de llamar a showHiddenPass si existe el elemento logineye
+    const logineye = document.getElementById('logineye');
+    if (logineye) {
+        showHiddenPass('loginpass', 'logineye');
+    }
+}
+if (nombrePagina == recovery) {
+    $(document).ready(function() {
+        $('#formularioLogin').on('submit', function(event) {
+            event.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+            
+            const email = $('#email').val(); // Obtener el valor del campo de correo electrónico
+
+            const requestData = {
+                email: email
+            };
+            
+            $.ajax({
+                url: 'https://paginas-web-cr.com/Api/apis/SendPassword.php', // URL de la API
+                type: 'POST',
+                contentType: 'application/json', // Tipo de contenido que se envía
+                data: JSON.stringify(requestData), // Datos a enviar, convertidos a JSON
+                success: function(response, textStatus, xhr) {
+                    if (xhr.status === 200) { // Si la respuesta es exitosa
+                        Swal.fire({
+                           
+                            position: 'center',
+                            icon: 'success',
+                            title: '¡Contraseña enviada!',
+                            showConfirmButton: false,
+                            timer: 3000
+                        }
+                    );
+                    } else {
+                        Swal.fire({
+                            position: 'center',
+                            icon: 'error',
+                            title: 'Usuario no encontrado',
+                            showConfirmButton: false,
+                            timer: 3000
+                        });
+                    }
+                },
+                error: function(xhr, status, error) {
+                    // Manejar errores de AJAX con SweetAlert
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'error',
+                        title: 'Error al enviar la solicitud',
+                        text: 'Por favor, inténtelo de nuevo más tarde.',
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+                }
+            });
+        });
+    });
+    
+
+}
